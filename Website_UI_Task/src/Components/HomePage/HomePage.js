7a4f1739377d52593b10ffc20372961590002963
Shashank Singh <101120408+Shankyking25@users.noreply.github.com>
2025-03-03 02:28:05 +0530

    Update HomePage.js

--- Website_UI_Task/src/Components/HomePage/HomePage.js
+++ Website_UI_Task/src/Components/HomePage/HomePage.js
@@ -13,6 +13,15 @@ import img7 from "../../Imgs/girl2.jpg"
 
 
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
 function HomePage(props) {
 
 
